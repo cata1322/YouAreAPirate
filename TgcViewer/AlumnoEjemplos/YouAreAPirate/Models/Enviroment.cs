37998f501b26219209fe1949867d2328dbb8e650
Vargas V Rodrigo <vargas.v.rodrigo@gmail.com>
2014-04-24 00:04:45 -0300

    INGRESE COMENTARIOS DE LOS PROCESOS, SOBRE LA FUNCIONALIDAD Y DESCRIPCION DE PASOS.

--- TgcViewer/AlumnoEjemplos/YouAreAPirate/Models/Enviroment.cs
+++ TgcViewer/AlumnoEjemplos/YouAreAPirate/Models/Enviroment.cs
@@ -13,15 +13,21 @@ using TgcViewer.Utils.TgcSceneLoader;
 namespace AlumnoEjemplos.YouAreAPirate
 {
     public partial class EjemploAlumno
-    {
+    {   
+        //Declaran las variables, para.
+        //Oceano
         TgcBox ocean;
+        //sol
         TgcSphere sun;
 
         public void initializeEnviroment()
         {
+            //Acceso a Device de DirectX.
             Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
 
+            
             TgcTexture waterTexture = TgcTexture.createTexture(d3dDevice, GuiController.Instance.AlumnoEjemplosMediaDir + "Textures\\water_texture.jpg");
+            
             ocean = TgcBox.fromSize(new Vector3(0, 0, 0), new Vector3(200, 0, 200), waterTexture);
 
             sun = new TgcSphere();
--- TgcViewer/AlumnoEjemplos/YouAreAPirate/Models/Ship.cs
+++ TgcViewer/AlumnoEjemplos/YouAreAPirate/Models/Ship.cs
@@ -13,20 +13,33 @@ namespace AlumnoEjemplos.YouAreAPirate
 {
     public partial class EjemploAlumno
     {
+        //variable que indica la velocidad con que se movera el barco.
         const float MOVEMENT_SPEED = 10f;
         TgcD3dInput input = GuiController.Instance.D3dInput;
         TgcBox ship;
 
         public void initializeShip()
         {
-            ship = TgcBox.fromSize(new Vector3(0, 0, 0), new Vector3(10,10,10), Color.Brown);
+            //Creacion del barco. Por ahora es una caja-cubo.
+            Vector3 center = new Vector3(0, 0, 0);
+            Vector3 size = new Vector3(10, 10, 10);
+            Color color = Color.Brown;
+            ship = TgcBox.fromSize(center,size, color);
+
+            //Ver tutorial5 para cargar un barco en 3d.
+
         }
 
+        
         public void moveShip(float elapsedTime)
         {
+            //proceso para mover el barco con las letras A,D,W,S.
+
+            //Declaramos un vector de movimiento, inicializado en 0.
             Vector3 movement = new Vector3(0, 0, 0);
 
-   
+            //El movimiento sobre el ocenao es en los ejes XZ.
+            //Movernos de izquierda a derecha, sobre el eje X.
             if (input.keyDown(Key.Left) || input.keyDown(Key.A))
             {
                 movement.X = 1;
@@ -35,7 +48,7 @@ namespace AlumnoEjemplos.YouAreAPirate
             {
                 movement.X = -1;
             }
-
+            //Movernos para adelante y atrás, sobre el eje Z.
             if (input.keyDown(Key.Up) || input.keyDown(Key.W))
             {
                 movement.Z = -1;
@@ -45,8 +58,10 @@ namespace AlumnoEjemplos.YouAreAPirate
                 movement.Z = 1;
             }
 
+            //Multiplicar movimiento por velocidad y elapsedTime
             movement *= MOVEMENT_SPEED * elapsedTime;
 
+            //Aplicar movimiento
             ship.move(movement);
         }
 
